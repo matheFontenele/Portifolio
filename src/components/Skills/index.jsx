53a1aca6af7ffd8b8f42a62b1f38a7bbe121dff3
Matheus <matheusfontenele979700@gmail.com>
2023-08-08 14:33:42 -0300

    portifolio

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,18 +1,23 @@
 import skills from 'assets/Json/skills.json'
+import styles from './Skills.module.scss'
+import { BsCodeSlash } from 'react-icons/bs'
 
 export default function Skills(){
     return(
-        <div>
-            <h1>Meus conhecimentos</h1>
+        <section className={styles.section}>
+            <h1 className={styles.section__title}><BsCodeSlash/>Meus conhecimentos</h1>  
+        
+            <div className={styles.skills}>
             {skills.map((skill) => (
-                <div key={skill.id}>
-                    <img src={skill.icon} alt={skill.nome}/>
-                    <div>
-                        <h2>{skill.nome}</h2>
-                        <p>{skill.sobre}<a href={skill.link}>Saiba Mais</a></p>
+                <div className={styles.skill} key={skill.id}>
+                    <img className={styles.skill__img} src={skill.icon} alt={skill.nome}/>
+                    <div className={styles.skill__div}>
+                        <h2 className={styles.skill__div__name}>{skill.nome}</h2>
+                        <p className={styles.skill__div__info}>{skill.sobre}<a className={styles.link} href={skill.link}>Saiba Mais</a></p>
                     </div>
                 </div>
             ))}
-        </div>
+            </div>
+        </section>
     )
 }
