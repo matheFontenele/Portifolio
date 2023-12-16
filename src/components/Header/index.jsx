36172898cd7c3991558b8d21dc7128dfdc7ef4fc
Matheus Fontenele <103459489+matheFontenele@users.noreply.github.com>
2023-12-16 19:14:56 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,16 @@
 import NavBar from 'components/NavBar'
 import styles from './Header.module.scss'
+import { Link } from 'react-router-dom'
 
 export default function Header(){
 
 
     return(
         <header className={styles.header}>
-            <span className={styles.header__nameMe}>
+            <Link className={styles.header__nameMe} to='/'>
                 <h1 className={styles.header__nameMe__matheus}>MATHEUS</h1>
                 <h1 className={styles.header__nameMe__fontenele}>FONTENELE</h1>
-            </span>
+            </Link>
 
             <NavBar/>
         
